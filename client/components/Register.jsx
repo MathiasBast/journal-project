608de29e2f670bb8ffac24675635a2f2f541f280
Mathias Bast <mathiasbast123@gmail.com>
2020-06-05 17:52:29 +1200

    idk

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,16 +1,89 @@
 import React from 'react'
 
 class Register extends React.Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: '',
+    conPassword: '',
+    matching: false
+  }
+
+  handleFirstNameChange = event => {
+    this.setState ({
+      firstName: event.target.value
+    })
+  }
+  handleLastNameChange = event => {
+    this.setState ({
+      lastName: event.target.value
+    })
+  }
+  handleEmailChange = event => {
+    this.setState ({
+      email: event.target.value
+    })
+  }
+  handleUsernameChange = event => {
+    this.setState ({
+      username: event.target.value
+    })
+  }
+  handlePasswordChange = event => {
+    this.setState ({
+      password: event.target.value
+    })
+  }
+  handleConPasswordChange = event => {
+    this.setState ({
+      conPassword: event.target.value
+    })
+    console.log(this.state.conPassword, this.state.password)
+    if(this.state.conPassword === this.state.password){
+      this.setState({
+        matching: true
+      })
+    } 
+    if(this.state.conPassword !== this.state.password) {
+      this.setState({
+        matching: false
+      })
+    }
+
+  }
+
   render () {
     return (
-      <div className='marginClass' >
-        <form>
-          <label>First Name</label><input type='text' />
-          <label>Last Name</label><input type='text' />
-          <label>Email</label><input type='text' />
-          <label>Username</label><input type='text' />
-          <label>Password</label><input type='text' />
-          <label>Confirmation Password</label><input type='text' />
+      <div className='marginClass register' >
+        <form >
+          <label>First Name</label>
+          <input autoComplete="off" className='register-input' type='text' value={this.state.firstName} onChange={this.handleFirstNameChange} />
+          <br />
+
+          <label>Last Name</label>
+          <input autoComplete="off" className='register-input' type='text' value={this.state.lastName} onChange={this.handleLastNameChange} />
+          <br />
+
+          <label>Email</label>
+          <input autoComplete="off" className='register-input' type='text' value={this.state.email} onChange={this.handleEmailChange} />
+          <br />
+
+          <label>Username</label>
+          <input autoComplete="off" className='register-input' type='text' value={this.state.username} onChange={this.handleUsernameChange} />
+          <br />
+
+          <label>Password</label>
+          <input autoComplete="off" className='register-input' type='password' value={this.state.password} onChange={this.handlePasswordChange} />
+          <br />
+
+          {this.state.matching && <p>Passwords do not match</p>}
+
+          <label>Confirmation Password</label>
+          <input autoComplete="off" className='register-input' type='password' value={this.state.conPassword} onChange={this.handleConPasswordChange} />
+
+          <button className='register-button' type='submit'>Submit</button>
         </form>
       </div>
     )
